bcdc7211d296cb41cedf7912b0d3d0a72fb7ba0d
kartheekreddy888333 <kartheekreddydalavai@gmail.com>
2025-05-19 00:27:58 +0530

    connectiog mongodb

--- Backend/server.js
+++ Backend/server.js
@@ -13,6 +13,9 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("server is running is sucessfully");
+});
 
 
 mongoose.connect(process.env.MONGO_URI, {
