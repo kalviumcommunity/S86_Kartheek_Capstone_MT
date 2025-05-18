34e4d54da383e8fce801236d84a3b10ce5300833
kartheekreddy888333 <kartheekreddydalavai@gmail.com>
2025-05-18 16:32:23 +0530

    Revert "implementing schemas"

--- Backend/models/Chef.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const chefSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  bio: String,
-  cuisineSpecialty: [String],
-});
-
-module.exports = mongoose.model("Chef", chefSchema);
